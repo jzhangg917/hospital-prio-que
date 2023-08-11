c5c501ffadfe1f375b41ffa0815a64b0e9811e8f
Jacky <94925733+jzhangg917@users.noreply.github.com>
2023-08-11 17:20:17 -0400

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,4 +1,5 @@
 from priority_queue import PriorityQueue
+import matplotlib.pyplot as plt
 
 class Patient:
     def __init__(self, name, condition, priority):
@@ -12,10 +13,20 @@ class Patient:
 class EmergencyRoom:
     def __init__(self):
         self.queue = PriorityQueue()
+        self.patient_counts = []
 
     def admit_patient(self, patient):
-        print(f"Admitting {patient} to the emergency room.")
-        self.queue.push(patient, patient.priority)
+        try:
+            if not isinstance(patient, Patient):
+                raise ValueError("Invalid patient object.")
+            
+            if not isinstance(patient.priority, int) or patient.priority <= 0:
+                raise ValueError("Priority must be a positive integer.")
+            
+            print(f"Admitting {patient} to the emergency room.")
+            self.queue.push(patient, patient.priority)
+        except ValueError as e:
+            print(f"Error: {e}")
 
     def treat_patient(self):
         if not self.queue.heap:
@@ -24,7 +35,15 @@ class EmergencyRoom:
 
         patient = self.queue.pop()
         print(f"Treating {patient}.")
+        self.patient_counts.append(len(self.queue.heap) + 1)
         return patient
+
+    def visualize_patient_flow(self):
+        plt.plot(range(len(self.patient_counts)), self.patient_counts, marker='o')
+        plt.xlabel('Time Intervals')
+        plt.ylabel('Number of Patients')
+        plt.title('Patient Flow in Emergency Room')
+        plt.show()
 
 if __name__ == "__main__":
     er = EmergencyRoom()
@@ -45,3 +64,5 @@ if __name__ == "__main__":
 
     treated_patient = er.treat_patient()
     print(f"Treated: {treated_patient}")
+
+    er.visualize_patient_flow()
